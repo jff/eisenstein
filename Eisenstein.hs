81c71d9a7a4e3cba36ddcb00e6daa2104893180c
jff <jff@adamastor.(none)>
2009-02-26 10:34:07 -0800

    Added a new version of function ei

--- Eisenstein.hs
+++ Eisenstein.hs
@@ -4,6 +4,15 @@ import Math.OEIS
 import Data.Maybe
 import Data.Ratio
 
+-- Newman's algorithm to list all positive rationals, 
+-- including 0%1 and 1%0
+newman :: [Rational]
+newman = iterate nextCW $ 1%1
+ where nextCW :: Rational -> Rational
+       nextCW r  =  let  (n,m)  = (numerator r, denominator r)
+                         j      = floor (n%m)
+                    in   m%((2*j+1)*m-n)
+
 
 -- Eisenstein function
 ei :: Integer -> Integer -> [Integer]
@@ -11,6 +20,14 @@ ei m n = m : eiloop 1 1 m n m n
  where eiloop a 1 m n cm cn = n : cm : eiloop 1 (a+1) cm (a*cm+cn) cm cn
        eiloop a b m n cm cn = let k = 2*floor(a%b)+1
                               in  n: eiloop b (k*b-a) n (k*n-m) cm cn
+
+-- Eisenstein function with a and b replaced by m and n in the calculation of k
+ei' :: Integer -> Integer -> [Integer]
+ei' m n = m : eiloop 1 1 m n m n
+ where eiloop a 1 m n cm cn = n : cm : eiloop 1 (a+1) cm (a*cm+cn) cm cn
+       eiloop a b m n cm cn = let k = 2*floor(m%n)+1
+                              in  n: eiloop b (k*b-a) n (k*n-m) cm cn
+
 
 -- Number of elements to test against OEIS
 numElems :: Int
